74a0982439cd959d34a87c2956d1697275c6acba
jwang1 <sigkdd@yahoo.com>
2018-10-21 15:44:17 -0400

    Android way of handling Image, setting background for an app is really lot of more work than iOS way
    1) beside the ImageView
    
    2) need to add assets directory, and saving the PNG files  there;  compared to those image-assets way creating resource (xml files)
    
    3) and then, creating an empty not used image-assert resource file, otherwise ImageView not able to add on layout
    
    4) now in the backend, controller, needs to use AssetManager to get asset by the file name, and then
       make InputFileStream, make a stream
       and then add that image to ImageView;  yeat, still not enough
    
    5) need to configure the ImageView with ScaleType of "fitXY" and
       "adjustViewBounds" and
       "cropToPadding"
    
       may not need to add both attributes, but they were selected when I was running trial-and-error, and finally got the background-png file to fill up the whole ImageView, either Portrait or Landscape mode.
    
    Note, iOS simply add the png file to x, 2x, 3x categories, and done.

--- app/src/main/java/com/iexpress/android/dicee/DiceeActivityFragment.java
+++ app/src/main/java/com/iexpress/android/dicee/DiceeActivityFragment.java
@@ -1,15 +1,26 @@
 package com.iexpress.android.dicee;
 
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DiceeActivityFragment extends Fragment {
+    private static final String TAG = "Dicee Activity Fragment";
+
+    private ImageView backgroundImageView;
 
     public DiceeActivityFragment() {
     }
@@ -17,6 +28,18 @@ public class DiceeActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dicee, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_dicee, container, false);
+
+        backgroundImageView = (ImageView) view.findViewById(R.id.backGroundImageView);
+        AssetManager am = getActivity().getAssets();
+        try (InputStream stream = am.open("newbackgroundLarge.png")) {
+            Drawable drawable = Drawable.createFromStream(stream, "newbackgroundLarge");
+            backgroundImageView.setImageDrawable(drawable);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading " + "newbackgroundLarge.png", e);
+        }
+
+        return view;
     }
 }
