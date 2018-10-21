2c170428661644d5405ff2ce1a14663e773410dc
jwang1 <sigkdd@yahoo.com>
2018-10-21 13:32:05 -0400

    merging remote master branch

---
+++ app/src/main/java/com/iexpress/android/dicee/DiceeActivityFragment.java
@@ -0,0 +1,22 @@
+package com.iexpress.android.dicee;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DiceeActivityFragment extends Fragment {
+
+    public DiceeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dicee, container, false);
+    }
+}
